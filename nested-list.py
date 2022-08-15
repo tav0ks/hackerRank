26008c23f2a7fdb579e691e7f0cdc5b2027d6dd4
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-08-15 06:52:04 -0300

    exercício Nested Lists

---
+++ nested-list.py
@@ -0,0 +1,23 @@
+#https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true
+notas = []
+records = {}
+for _ in range(int(raw_input())):
+    name = raw_input()
+    score = float(raw_input())
+    
+    records[name] = score
+    notas.append(score)
+notas.sort()
+slg = 0
+names = []
+for i in notas:
+    if i > notas[0]:
+        slg = i
+        break
+
+for i in records.items():
+    if i[1] == slg:
+        names.append(i[0])
+names.sort()
+for i in names:
+    print(i)
