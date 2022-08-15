485cafa00136b6b3d35d826858e097dfcb938224
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-08-15 04:37:47 -0300

    exercício write a function

---
+++ write_a_function.py
@@ -0,0 +1,14 @@
+#https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0 :
+        leap = True
+        if year % 100 == 0:
+            leap = False
+            if year % 400 == 0:
+                leap = True
+                return leap
+            return leap
+        return leap
+    return leap
